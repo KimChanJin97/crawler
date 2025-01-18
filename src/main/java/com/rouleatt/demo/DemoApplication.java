3fbc0dc1e389114431e4dbe90e9aabfbbf2a8145
KimChanJin97 <cjkimhello97@gmail.com>
2025-01-18 21:21:50 +0900

    chore: DemoApplication 푸시

---
+++ src/main/java/com/rouleatt/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.rouleatt.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@RequiredArgsConstructor
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
