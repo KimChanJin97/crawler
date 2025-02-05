41e7e8e5e293c0a47c006d448ca75d0ffd379234
KimChanJin97 <cjkimhello97@gmail.com>
2025-02-06 01:29:54 +0900

    fix: 크롤링 로직에서 파일 쓰기 로직 분리

---
+++ src/main/java/com/rouleatt/demo/writer/MenuReviewWriter.java
@@ -0,0 +1,93 @@
+package com.rouleatt.demo.writer;
+
+import static com.rouleatt.demo.utils.CrawlerUtils.DELIMITER;
+
+import com.rouleatt.demo.dto.MenuDto;
+import com.rouleatt.demo.dto.ReviewDto;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class MenuReviewWriter {
+
+    private static final String MENU_FILE_NAME_POSTFIX = "_menu.csv";
+    private static final String REVIEW_FILE_NAME_POSTFIX = "_review.csv";
+
+    public void writeMenu(String engName, List<MenuDto> menuDtos) {
+
+        File file = new File(engName.concat(MENU_FILE_NAME_POSTFIX));
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            if (file.length() == 0) {
+                // CSV 헤더 작성
+                StringBuilder sb = new StringBuilder()
+                        .append("restaurant_id").append(DELIMITER)
+                        .append("menu_index").append(DELIMITER)
+                        .append("name").append(DELIMITER)
+                        .append("is_recommended").append(DELIMITER)
+                        .append("price").append(DELIMITER)
+                        .append("description").append(DELIMITER)
+                        .append("image");
+                writer.write(sb.toString());
+                writer.newLine();
+            }
+            // CSV 칼럼 작성
+            for (MenuDto menuDto : menuDtos) {
+                StringBuilder sb = new StringBuilder()
+                        .append(menuDto.restaurantId()).append(DELIMITER)
+                        .append(menuDto.menuIndex()).append(DELIMITER)
+                        .append(menuDto.name()).append(DELIMITER)
+                        .append(menuDto.isRecommended()).append(DELIMITER)
+                        .append(menuDto.price()).append(DELIMITER)
+                        .append(menuDto.description()).append(DELIMITER)
+                        .append(menuDto.image());
+                writer.write(sb.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeReview(String engName, List<ReviewDto> reviewDtos) {
+
+        File file = new File(engName.concat(REVIEW_FILE_NAME_POSTFIX));
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            if (file.length() == 0) {
+                // CSV 헤더 작성
+                StringBuilder sb = new StringBuilder()
+                        .append("restaurant_id").append(DELIMITER)
+                        .append("reviewer_name").append(DELIMITER)
+                        .append("type_name").append(DELIMITER)
+                        .append("url").append(DELIMITER)
+                        .append("thumbnail_url").append(DELIMITER)
+                        .append("title").append(DELIMITER)
+                        .append("review_index").append(DELIMITER)
+                        .append("content").append(DELIMITER)
+                        .append("created_at");
+                writer.write(sb.toString());
+                writer.newLine();
+            }
+            // CSV 칼럼 작성
+            for (ReviewDto reviewDto : reviewDtos) {
+                StringBuilder sb = new StringBuilder()
+                        .append(reviewDto.restaurantId()).append(DELIMITER)
+                        .append(reviewDto.reviewerName()).append(DELIMITER)
+                        .append(reviewDto.typeName()).append(DELIMITER)
+                        .append(reviewDto.url()).append(DELIMITER)
+                        .append(reviewDto.thumbnailUrl()).append(DELIMITER)
+                        .append(reviewDto.title()).append(DELIMITER)
+                        .append(reviewDto.reviewIndex()).append(DELIMITER)
+                        .append(reviewDto.content().substring(0, 100)).append(DELIMITER)
+                        .append(reviewDto.createdAt());
+                writer.write(sb.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/rouleatt/demo/writer/RestaurantImageWriter.java
@@ -0,0 +1,79 @@
+package com.rouleatt.demo.writer;
+
+import static com.rouleatt.demo.utils.CrawlerUtils.DELIMITER;
+
+import com.rouleatt.demo.dto.ImageDto;
+import com.rouleatt.demo.dto.RestaurantDto;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Set;
+
+public class RestaurantImageWriter {
+    private static final String RESTAURANT_FILE_NAME_PREFIX = "";
+    private static final String RESTAURANT_FILE_NAME_POSTFIX = "_restaurant.csv";
+    private static final String IMAGE_FILE_NAME_POSTFIX = "_image.csv";
+
+    public void writerRestaurant(String engName, Set<RestaurantDto> restaurantDtos) {
+
+        File file = new File(engName.concat(RESTAURANT_FILE_NAME_POSTFIX));
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+            if (file.length() == 0) {
+                // CSV 헤더 작성
+                StringBuilder sb = new StringBuilder()
+                        .append("id").append(DELIMITER)
+                        .append("name").append(DELIMITER)
+                        .append("x").append(DELIMITER)
+                        .append("y").append(DELIMITER)
+                        .append("category").append(DELIMITER)
+                        .append("address").append(DELIMITER)
+                        .append("road_address");
+                bw.write(sb.toString());
+                bw.newLine();
+            }
+            // CSV 칼럼 작성
+            for (RestaurantDto restaurantDto : restaurantDtos) {
+                StringBuilder sb = new StringBuilder()
+                        .append(restaurantDto.id()).append(DELIMITER)
+                        .append(restaurantDto.name()).append(DELIMITER)
+                        .append(restaurantDto.x()).append(DELIMITER)
+                        .append(restaurantDto.y()).append(DELIMITER)
+                        .append(restaurantDto.category()).append(DELIMITER)
+                        .append(restaurantDto.address()).append(DELIMITER)
+                        .append(restaurantDto.roadAddress());
+                bw.write(sb.toString());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeImage(String engName, Set<ImageDto> imageDtos) {
+
+        File file = new File(engName.concat(IMAGE_FILE_NAME_POSTFIX));
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+            if (file.length() == 0) {
+                // CSV 헤더 작성
+                StringBuilder sb = new StringBuilder()
+                        .append("restaurant_id").append(DELIMITER)
+                        .append("url");
+                bw.write(sb.toString());
+                bw.newLine();
+            }
+            // CSV 칼럼 작성
+            for (ImageDto imageDto : imageDtos) {
+                StringBuilder sb = new StringBuilder()
+                        .append(imageDto.restaurantId()).append(DELIMITER)
+                        .append(imageDto.url());
+                bw.write(sb.toString());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
