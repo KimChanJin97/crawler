9fe401b6c960485d8649f692b2751239c2fee595
KimChanJin97 <cjkimhello97@gmail.com>
2025-02-14 10:23:09 +0900

    feat: decodeUnicode() 구현

--- src/main/java/com/rouleatt/demo/utils/CrawlerUtils.java
+++ src/main/java/com/rouleatt/demo/utils/CrawlerUtils.java
@@ -1,10 +1,20 @@
 package com.rouleatt.demo.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CrawlerUtils {
 
-    public static final String DELIMITER = "DELIMITER";
-    public static final String RESTAURANT_FILE_POSTFIX = "_restaurant.csv";
-    public static final String IMAGE_FILE_POSTFIX = "_image.csv";
-    public static final String MENU_FILE_POSTFIX = "_menu.csv";
-    public static final String REVIEW_FILE_POSTFIX = "_review.csv";
+    public static String decodeUnicode(String input) {
+        Pattern pattern = Pattern.compile("\\\\u([0-9A-Fa-f]{4})");
+        Matcher matcher = pattern.matcher(input);
+        StringBuffer buffer = new StringBuffer();
+
+        while (matcher.find()) {
+            int unicodeValue = Integer.parseInt(matcher.group(1), 16); // 16진수
+            matcher.appendReplacement(buffer, Character.toString((char) unicodeValue));
+        }
+        matcher.appendTail(buffer);
+        return buffer.toString();
+    }
 }
