f29478848ec224a4bac6c5e736e68a61601dea99
KimChanJin97 <cjkimhello97@gmail.com>
2025-02-16 13:59:59 +0900

    feat: User-Agent 랜덤 설정 메서드 구현

--- src/main/java/com/rouleatt/demo/utils/CrawlerUtils.java
+++ src/main/java/com/rouleatt/demo/utils/CrawlerUtils.java
@@ -1,9 +1,17 @@
 package com.rouleatt.demo.utils;
 
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CrawlerUtils {
+
+    public static final String USER_AGENT_KEY = EnvLoader.get("USER_AGENT_KEY");
+    private static final String[] USER_AGENT_VALUES = {
+            EnvLoader.get("USER_AGENT_WINDOW_VALUE"),
+            EnvLoader.get("USER_AGENT_MACINTOSH_VALUE"),
+            EnvLoader.get("USER_AGENT_UBUNTU_VALUE")
+    };
 
     public static String decodeUnicode(String input) {
         Pattern pattern = Pattern.compile("\\\\u([0-9A-Fa-f]{4})");
@@ -16,5 +24,9 @@ public class CrawlerUtils {
         }
         matcher.appendTail(buffer);
         return buffer.toString();
+    }
+
+    public static String getUserAgentValue() {
+        return USER_AGENT_VALUES[new Random().nextInt(USER_AGENT_VALUES.length)];
     }
 }
