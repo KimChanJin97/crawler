1b6e18b391eea213954b505a15d87c7b6d5dc597
KimChanJin97 <cjkimhello97@gmail.com>
2025-03-05 00:51:24 +0900

    fix: TableManager init() 삭제

--- src/main/java/com/rouleatt/demo/CrawlerApplication.java
+++ src/main/java/com/rouleatt/demo/CrawlerApplication.java
@@ -1,14 +1,10 @@
 package com.rouleatt.demo;
 
 import com.rouleatt.demo.crawler.RestaurantImageBatchCrawler;
-import com.rouleatt.demo.db.TableManager;
 
 public class CrawlerApplication {
 
     public static void main(String[] args) {
-
-        TableManager tableManager = new TableManager();
-        tableManager.init();
 
         RestaurantImageBatchCrawler crawler = new RestaurantImageBatchCrawler();
         crawler.crawl();
